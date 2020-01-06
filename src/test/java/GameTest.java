abc025c4b5f264ef7d93c1d42b022472cdea83a4
lfresco <loryfresco@gmail.com>
2020-01-06 11:13:58 +0100

    add size8 test

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,13 +1,12 @@
 import it.freedom.Game;
-
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class GameTest {
-    
-    private String emptyBoard = "|_|_|_|_|_|_|_|_|_|_|\n" +
+
+    private String emptyBoard10 = "|_|_|_|_|_|_|_|_|_|_|\n" +
             "|_|_|_|_|_|_|_|_|_|_|\n" +
             "|_|_|_|_|_|_|_|_|_|_|\n" +
             "|_|_|_|_|_|_|_|_|_|_|\n" +
@@ -17,11 +16,30 @@ public class GameTest {
             "|_|_|_|_|_|_|_|_|_|_|\n" +
             "|_|_|_|_|_|_|_|_|_|_|\n" +
             "|_|_|_|_|_|_|_|_|_|_|";
-    
+
+    private String emptyBoard8 = "|_|_|_|_|_|_|_|_|\n" +
+            "|_|_|_|_|_|_|_|_|\n" +
+            "|_|_|_|_|_|_|_|_|\n" +
+            "|_|_|_|_|_|_|_|_|\n" +
+            "|_|_|_|_|_|_|_|_|\n" +
+            "|_|_|_|_|_|_|_|_|\n" +
+            "|_|_|_|_|_|_|_|_|\n" +
+            "|_|_|_|_|_|_|_|_|" ;
+
+
+
     @Test
-    public void isEmptyBoard(){
+    public void isEmptyBoard10(){
         Game game = new Game(10);
-        assertThat(game.toString(), is(emptyBoard));
+        assertThat(game.toString(), is(emptyBoard10));
 
     }
+
+    @Test
+    public void isEmptyBoard8(){
+        Game game = new Game(8);
+        assertThat(game.toString(), is(emptyBoard8));
+    }
+
+    
 }
