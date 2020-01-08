2835abecf007dcf658826c6dfcc16f2ff31baa7b
lfresco <loryfresco@gmail.com>
2020-01-08 15:34:29 +0100

    finished board

--- src/main/java/freedom/Board.java
+++ src/main/java/freedom/Board.java
@@ -1,14 +1,5 @@
 package freedom;
 
-import checkers.AdjacentChecker;
-import checkers.BoundsChecker;
-import checkers.MoveValidator;
-import checkers.OccupiedChecker;
-import exceptions.NonAdjacentException;
-import exceptions.OccupiedCellException;
-import exceptions.OutOfBoundsException;
-
-import java.awt.event.AdjustmentEvent;
 import java.util.stream.IntStream;
 
 class Board {
@@ -16,9 +7,7 @@ class Board {
     private Character[][] currentBoard;
     private int boardSize;
     private char emptyCellCharacter = '_';
-    private BoundsChecker boundsChecker;
-    private MoveValidator moveValidator;
- 
+
     Board(int boardSize) {
 
         this.boardSize = boardSize;
@@ -26,35 +15,25 @@ class Board {
         IntStream.range(0, boardSize)
                 .forEach(r -> IntStream.range(0, boardSize)
                         .forEach(c -> currentBoard[r][c] = emptyCellCharacter));
-        
-        this.boundsChecker = new BoundsChecker(boardSize);
-        this.moveValidator = new MoveValidator(emptyCellCharacter);
-        
     }
     
     Character[][] getCurrentBoard() {
+
         return this.currentBoard;
     }
     
-    private Character getStone(Integer row, Integer column) {
+    Character getStone(Integer row, Integer column) {
+
         return currentBoard[row-1][column-1];
     }
     
     void setStone(Character symbol, int row, int column) {
-        try {
-            boundsChecker.boundsCheck(row);
-            boundsChecker.boundsCheck(column);
-            Character currentStone = getStone(row, column);
-            moveValidator.validateMove(row, column,currentStone);
-            currentBoard[row-1][column-1] = symbol;
-            
-            
-        } catch (OutOfBoundsException | NonAdjacentException | OccupiedCellException e) {
-            System.out.println(e.getMessage());
-        }
+
+        currentBoard[row-1][column-1] = symbol;
     }
 
     Character getEmptyCellCharacter() {
+
         return emptyCellCharacter;
     }
 }
--- src/main/java/freedom/Game.java
+++ src/main/java/freedom/Game.java
@@ -1,13 +1,23 @@
 package freedom;
 
+import checkers.BoundsChecker;
+import checkers.MoveValidator;
+import exceptions.NonAdjacentException;
+import exceptions.OccupiedCellException;
+import exceptions.OutOfBoundsException;
+
 public class Game {
     
     private Board board;
     private Character currentStone;
+    private MoveValidator moveValidator;
+    private BoundsChecker boundsChecker;
 
     public Game(int size) {
         this.board = new Board(size);
         this.currentStone = 'W';
+        this.boundsChecker = new BoundsChecker(size);
+        this.moveValidator = new MoveValidator(board.getEmptyCellCharacter());
     }
 
     @Override
@@ -16,8 +26,19 @@ public class Game {
     }
 
     public void move(Integer row, Integer column) {
-        board.setStone(currentStone, row, column);
-        currentStone = nextPlayer();
+        try {
+            boundsChecker.boundsCheck(row);
+            boundsChecker.boundsCheck(column);
+            Character cellStone = board.getStone(row, column);
+            moveValidator.validateMove(row, column,cellStone);
+            board.setStone(currentStone, row, column);
+            currentStone = nextPlayer();
+
+        }catch (OutOfBoundsException | NonAdjacentException | OccupiedCellException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
     
     private Character nextPlayer() {
