a4b9dad6d8a6888f6f9b0de52dde98d8d34ea07d
lfresco <loryfresco@gmail.com>
2020-01-09 09:27:19 +0100

    refactoring

--- src/test/java/FreedomMoveTest.java
+++ src/test/java/FreedomMoveTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class FreedomTest {
+public class FreedomMoveTest {
 
     private String emptyBoardSizeTen = "|_|_|_|_|_|_|_|_|_|_|\n" +
             "|_|_|_|_|_|_|_|_|_|_|\n" +
