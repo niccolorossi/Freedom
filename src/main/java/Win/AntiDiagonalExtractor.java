a2b8d619a4fdcb7a358836edd628a004594e3082
lfresco <loryfresco@gmail.com>
2020-01-10 12:39:42 +0100

    passed AntiDiagonal Tests

---
+++ src/main/java/Win/AntiDiagonalExtractor.java
@@ -0,0 +1,44 @@
+package Win;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AntiDiagonalExtractor {
+
+
+    public static List<Character[]> extractAntiDiagonals(Character[][] fullBoard) {
+
+        List<Character[]> toReturn = new ArrayList<>();
+        List<Character[]> upperDiagonalsList = new ArrayList<>();
+        List<Character[]> lowerDiagonalsList = new ArrayList<>();
+        Integer size = fullBoard[0].length;
+        Integer numberOfUpperDiagonals = size-4;
+
+        for(int d=1; d<=numberOfUpperDiagonals; d++) {
+            int col;
+            Character[] upperDiagonalAsArray = new Character[size-d];
+            Character[] lowerDiagonalAsArray = new Character[size-d];
+            for(int row=0; row<size-d; row++) {
+                col = row+d;
+                lowerDiagonalAsArray[row] = fullBoard[row+d][size-row-1];
+                upperDiagonalAsArray[row] = fullBoard[row][size-row-1-d];
+            }
+            upperDiagonalsList.add(upperDiagonalAsArray);
+            lowerDiagonalsList.add(lowerDiagonalAsArray);
+        }
+        toReturn.addAll(upperDiagonalsList);
+        toReturn.addAll(lowerDiagonalsList);
+        toReturn.add(extractMainAntiDiagonal(fullBoard));
+        return toReturn;
+    }
+
+    private static Character[] extractMainAntiDiagonal(Character[][] fullBoard) {
+
+        Integer size = fullBoard[0].length;
+        Character[] toReturn = new Character[size];
+        for(int row=0; row<size; row++) {
+            toReturn[row] = fullBoard[row][size-row-1];
+        }
+        return toReturn;
+    }
+}
---
+++ src/test/java/AntiDiagonalTest.java
@@ -0,0 +1,57 @@
+import Win.AntiDiagonalExtractor;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AntiDiagonalTest {
+
+    private Character[][] fullBoard;
+    private Character[] mainAntiDiagonal;
+    private Character[] firstUpperAntiDiagonal;
+    private Character[] thirdUpperAntiDiagonal;
+    private Character[] thirdLowerAntiDiagonal;
+
+    @Before
+    public void initFullBoard(){
+
+        fullBoard = new Character[][]
+                        {{'B', 'W', 'B', 'W', 'B', 'B', 'W', 'W', 'B', 'W'},
+                        {'B', 'W', 'B', 'B', 'W', 'W', 'B', 'B', 'B', 'W'},
+                        {'W', 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B'},
+                        {'B', 'B', 'W', 'B', 'B', 'W', 'B', 'W', 'W', 'W'},
+                        {'W', 'W', 'B', 'W', 'W', 'W', 'B', 'B', 'W', 'B'},
+                        {'B', 'B', 'W', 'B', 'B', 'W', 'W', 'B', 'B', 'W'},
+                        {'W', 'W', 'B', 'W', 'W', 'B', 'B', 'W', 'B', 'B'},
+                        {'W', 'W', 'B', 'W', 'W', 'B', 'W', 'W', 'W', 'W'},
+                        {'B', 'B', 'B', 'B', 'W', 'W', 'B', 'B', 'B', 'B'},
+                        {'W', 'B', 'W', 'W', 'B', 'B', 'W', 'W', 'W', 'B'}};
+
+        mainAntiDiagonal = new Character[] {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'B', 'W'};
+        firstUpperAntiDiagonal = new Character[] {'B', 'B', 'B', 'W', 'W', 'B', 'B', 'W', 'B'};
+        thirdUpperAntiDiagonal = new Character[] {'W', 'W', 'B', 'B', 'B', 'B', 'W'};
+        thirdLowerAntiDiagonal = new Character[] {'W', 'W', 'B', 'B', 'B', 'W', 'W'};
+    }
+
+    @Test
+    public void mainDiagonalExtractor() {
+        List<Character[]> diagonals = AntiDiagonalExtractor.extractAntiDiagonals(fullBoard);
+        assertThat(diagonals.get(12), is(mainAntiDiagonal));
+    }
+
+    @Test
+    public void thirdUpperDiagonalExtractor() {
+        List<Character[]> diagonals = AntiDiagonalExtractor.extractAntiDiagonals(fullBoard);
+        assertThat(diagonals.get(2), is(thirdUpperAntiDiagonal));
+    }
+
+    @Test
+    public void thirdLowerDiagonalExtractor() {
+        List<Character[]> diagonals = AntiDiagonalExtractor.extractAntiDiagonals(fullBoard);
+        assertThat(diagonals.get(8), is(thirdLowerAntiDiagonal));
+    }
+
+}
