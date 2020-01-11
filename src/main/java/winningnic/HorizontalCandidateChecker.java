327ee9f64df229fb27311ce98706023eb280d5c0
Niccolò <rossinikolai@gmail.com>
2020-01-11 15:27:23 +0100

    wrote HorizontalCandidateChecker for easiest test

---
+++ src/main/java/winningnic/HorizontalCandidateChecker.java
@@ -0,0 +1,15 @@
+package winningnic;
+
+public class HorizontalCandidateChecker {
+    
+    private static final Integer nextQuadrupletOffset = 4;
+    private Boolean isCandidate;
+    
+    public HorizontalCandidateChecker(Character[][] fullBoard, Integer beginRow, Integer beginColumn) {
+        this.isCandidate = (fullBoard[beginRow][beginColumn] == fullBoard[beginRow+nextQuadrupletOffset][beginColumn]);
+    }
+    
+    public Boolean getIsCandidate() {
+        return this.isCandidate;
+    }
+}
